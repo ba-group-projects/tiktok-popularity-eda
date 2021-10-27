e393bfaf46326fc8c8a5530329e7b5fce9f1f2fd
mattlim96-Desktop <mattlim96@gmail.com>
2021-10-28 00:36:10 +0100

    Add functions to clean and summarize data

--- clean.py
+++ clean.py
@@ -16,20 +16,53 @@ class CleanData:
     def add_comment_rate(self):
         self.dfm["commentRate"] = self.dfm["commentCount"] / self.dfm["playCount"]
 
+    def unix_to_datetime(self):
+        self.dfm['createTime'] = pd.to_datetime(self.dfm['createTime'], unit='s')
+
+    def drop_useless_col(self, colArr):
+        self.dfm.drop(colArr, axis=1, inplace=True)
+
+    def rename_col(self, renameDict):
+        self.dfm.rename(columns=renameDict, inplace=True)
+
+    def check_missing_data(self):
+        columns = self.dfm.columns
+        countNull = self.dfm.isnull().sum()
+        
+        if sum(countNull) == 0:
+            print('Missing Data:\n=============\nNone\n')
+        else:
+            for index in range(len(columns)):
+                if countNull[index] > 0:
+                    print(f'Missing Data: {columns[index]} column has {countNull[index]} missing values\n')
+
+    def get_time_period(self):
+        minDate = min(self.dfm['createTime'])
+        maxDate = max(self.dfm['createTime'])
+        timeDiff = maxDate - minDate
+        print(f'Data Time Period:\n=================\nStart Date: {minDate}\n  End Date: {maxDate}\n Timedelta: {timeDiff}')
+
     def process_the_data(self):
         self.add_share_rate()
         self.add_dig_rate()
         self.add_comment_rate()
+        self.unix_to_datetime()
+        self.rename_col({"diggCount": "likeCount"})
+        self.drop_useless_col(['videoUrl', 'videoUrlNoWaterMark'])
+
+    def summary_of_data(self):
+        self.check_missing_data()
+        self.get_time_period()
 
     def save_cleaned_data(self):
         self.dfm.to_csv("data/cleaned_data.csv")
 
 
 if __name__ == '__main__':
-    data = json.load(open('../archive/trending.json', encoding="utf8"))
+    data = json.load(open('./data/trending.json', encoding="utf8"))
     data = data['collector']
-    df = pd.DataFrame(data)
+    df = pd.json_normalize(data)
     clean_data = CleanData(df)
     clean_data.process_the_data()
+    clean_data.summary_of_data()
     clean_data.save_cleaned_data()
-
